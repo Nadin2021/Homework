e674973fd6e9da46055bd2f92ea6195220c3419f
Nadezhda <8987813@mail.ru>
2023-01-30 00:33:50 +0300

    DZ seminar1 Csharp

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*
+Console.Write ( "Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ( "Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+
+Console. Write ("max= ");
+Console.WriteLine(max);
+---------------------------------------------------------------------
+*/
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+/*
+Console.Write ( "Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ( "Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ( "Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console. Write ("max= ");
+Console.WriteLine(max);
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+/*
+
+Console.Write ( "Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+    if ( a % 2 == 0)
+    {
+     Console.WriteLine ("Да");
+    }
+    else
+    {
+     Console.WriteLine ("Нет");  
+    }
+   
+    ----------------------------------------------------------------
+    */
+
+    //Задача 8: Напишите программу, которая на вход принимает число (N),
+    // а на выходе показывает все чётные числа от 1 до N.
+
+    /*Console.Write ( "Введите число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int current = 2;
+
+    while (current <= n)
+    {
+        Console.Write (current + " ");
+        current += 2;
+    }
+    */
+
+    
+
+    
